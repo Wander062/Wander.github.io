371c4567d0613ac7251df4b08a7823f021551481
Wander G. <165552397+Wander062@users.noreply.github.com>
2025-04-16 21:23:58 -0300

    script.js

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-let display = document.getElementById('display');
-
+let display = document.getElementById("display");
 function adicionarValor(valor) {
-    if (display.value === 'Erro') limpar();
     display.value += valor;
 }
-
-function limpar() {
-    display.value = '';
-}
-
 function calcular() {
     try {
-        let expressao = display.value
-            .replace(/×/g, '*')
-            .replace(/%/g, '/100');
-        
-        let resultado = eval(expressao);
-        
-        if (isNaN(resultado) || !isFinite(resultado)) {
-            throw new Error();
-        }
-        
-        display.value = resultado;
-    } catch (error) {
-        display.value = 'Erro';
-        setTimeout(limpar, 1000);
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
     }
 }
