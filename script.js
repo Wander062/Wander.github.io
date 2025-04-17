2726414a28fae425e681ded924a3aacf87c9e7c5
Wander G. <165552397+Wander062@users.noreply.github.com>
2025-04-16 21:20:32 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-let display = document.getElementById("display");
+let display = document.getElementById('display');
+
 function adicionarValor(valor) {
+    if (display.value === 'Erro') limpar();
     display.value += valor;
+}
+
+function limpar() {
+    display.value = '';
+}
+
+function calcular() {
+    try {
+        let expressao = display.value
+            .replace(/×/g, '*')
+            .replace(/%/g, '/100');
+        
+        let resultado = eval(expressao);
+        
+        if (isNaN(resultado) || !isFinite(resultado)) {
+            throw new Error();
+        }
+        
+        display.value = resultado;
+    } catch (error) {
+        display.value = 'Erro';
+        setTimeout(limpar, 1000);
+    }
 }
